9d530aa1e6948ffb511fd51f49b44ec6d1470f08
SujalLama <lsujal41@gmail.com>
2025-04-25 17:53:25 +0545

    book-vanilla-js todo app

---
+++ vanilla-todo-book/src/main.ts
@@ -0,0 +1,113 @@
+import "./style.css";
+
+// state of the app
+const todos: string[] = ["Walk the dog", "Water the plants", "Sand the chairs"];
+
+// HTML element references
+const addTodoInput: HTMLElement | null = document.getElementById("todo-input");
+const addTodoButton: HTMLElement | null =
+  document.getElementById("add-todo-btn");
+const todosList = document.getElementById("todos-list");
+
+// Initialize the view
+for (const todo of todos) {
+  todosList.append(renderTodoInReadMode(todo));
+}
+
+addTodoInput?.addEventListener("input", () => {
+  addTodoButton.disabled = addTodoInput?.value.length < 3;
+});
+
+addTodoInput?.addEventListener("keydown", ({ key }) => {
+  if (key === "Enter" && addTodoInput?.value.length >= 3) {
+    addTodo();
+  }
+});
+
+addTodoButton?.addEventListener("click", () => {
+  addTodo();
+});
+
+// Functions
+function renderTodoInReadMode(todo: string) {
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = todo;
+  span.addEventListener("dblclick", () => {
+    const idx = todos.indexOf(todo);
+
+    todosList?.replaceChild(
+      renderTodoInEditMode(todo),
+      todosList.childNodes[idx],
+    );
+  });
+
+  li.append(span);
+  const button = document.createElement("button");
+  button.textContent = "Done";
+  button.addEventListener("click", () => {
+    const idx = todos.indexOf(todo);
+    removeTodo(idx);
+  });
+  li.append(button);
+  return li;
+}
+
+function renderTodoInEditMode(todo: string) {
+  const li = document.createElement("li");
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = todo;
+  li.append(input);
+
+  const saveBtn = document.createElement("button");
+  saveBtn.textContent = "Save";
+  saveBtn.addEventListener("click", () => {
+    const idx = todos.indexOf(todo);
+    updateTodo(idx, input.value);
+  });
+
+  li.append(saveBtn);
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.textContent = "Cancel";
+  cancelBtn.addEventListener("click", () => {
+    const idx = todos.indexOf(todo);
+    todosList?.replaceChild(
+      renderTodoInReadMode(todo),
+      todosList.childNodes[idx],
+    );
+  });
+  li.append(cancelBtn);
+
+  return li;
+}
+
+function addTodo() {
+  const description = addTodoInput.value;
+
+  if (todos.includes(description)) {
+    alert("Todo already exist");
+    return;
+  }
+
+  todos.push(description);
+  const todo = renderTodoInReadMode(description);
+  todosList.append(todo);
+
+  addTodoInput.value = "";
+  addTodoButton.disabled = true;
+}
+
+function removeTodo(index) {
+  // todos.splice(index, 1);
+  todosList.childNodes[index].style.textDecoration = "line-through";
+}
+
+function updateTodo(index, description) {
+  todos[index] = description;
+  const todo = renderTodoInReadMode(description);
+  todosList?.replaceChild(todo, todosList.childNodes[index]);
+}
